f1f672be35eab6593c31c35c137eaf5437e2c702
wwTsosww <117574328+wwTsosww@users.noreply.github.com>
2022-12-28 09:32:11 +0700

    project
    1. index.html
    2. Gun.html
    3. Ammo.html
    4. Promotion.html
    5. Target.html
    6. styles.css
    7. index.js

---
+++ index.js
@@ -0,0 +1,394 @@
+var home =[{
+    id: 1,
+    img:'images\1.jpg',
+    name:'GLOCK 19 Gen5',
+    price:52000,
+    description:'A popular model and a popular brand from Austria, GLOCK, a well-known gun manufacturer that constantly releases new models',
+    type:'topgun'
+},{
+    id: 2,
+    img:'images\2.png',
+    name:'MP153',
+    price:28000,
+    description:'Shotguns with automatic systems are manufactured at Russia Manufactured in a factory in Russia as well. And most importantly, this gun has a shock attached to the barrel',
+    type:'topgun'
+},{
+    id: 3,
+    img:'images\3.jpg',
+    name:'Barrett M82 .50 Cal',
+    price:95000,
+    description:'A long-range sniper with high firepower. Developed by Barrett Firearms USA, this sniper rifle is stationed in many units around the world',
+    type:'topgun'
+},{
+    id: 4,
+    img:'images\4.jpg',
+    name:'L115A3 AWM (AWM-Arctic Warfare Magnum)',
+    price:65000,
+    description:'Country of origin Britain was used in the Afghanistan War, Iraq War',
+    type:'topgun'
+},{
+    id: 5,
+    img:'images\5.jpg',
+    name:'Steyr AUG A3 M1',
+    price:55000,
+    description:'Bullpup assault rifles, or those whose mechanism is located at the rear of the trigger, or in other words, the gun with the magazine behind it. A design like this that we should see each other often and a unique design like this is not just for beauty',
+    type:'topgun'
+},{
+    id: 6,
+    img:'images\6.jpg',
+    name:'Heckler & Koch HK MR',
+    price:65000,
+    description:'The shape that we may come across often that we often collectively refer to as M4',
+    type:'topgun'
+},{
+    id: 7,
+    img:'images-g\1.jpg',
+    name:'GLOCK 17',
+    price:52000,
+    description:'The last model, used from childhood to growing up, Glock 17, an Austrian product that serves both in movies and in real life. ',
+    type:'Pistol'
+},{
+    id: 8,
+    img:'images-g\2.jpg',
+    name:'Sig Sauer P320 Compact',
+    price:7500,
+    description:'P320 from Sig Sauer, British national again. The trigger is made to increase the accuracy of shooting more.',
+    type:'Pistol'
+},{
+    id: 9,
+    img:'images-g\3.jpg',
+    name:'Heckler & Koch USP Compact',
+    price:78000,
+    description:'The USP Compact is a compact version with a small gun chambered for a variety of 9mm, .40 or .45 ACP (larger cartridges).',
+    type:'Pistol'
+},{
+    id: 10,
+    img:'images-g\4.jpg',
+    name:'Sig Sauer P226 RX',
+    price:95000,
+    description:'P226 from UK From the makers of Sig Sauer, this handgun barrel is often seen in many FPS or TPS shooting games.',
+    type:'Pistol'
+},{
+    id:11,
+    img:'images-g\5.jpg',
+    name:'Kei-Tec KSG',
+    price:85000,
+    description:'It can be called the best shotgun in the world at this time. Its a pump-action gun with a 12-gauge pictorial barrel.',
+    type:'shotgun'
+},{
+    id: 12,
+    img:'images-g\6.jpg',
+    name:'SRMARMS model 1216',
+    price:87275,
+    description:'Its a system-operated gun. semiautomatic It has a detachable magazine that fires 16 rounds per reload.',
+    type:'shotgun'
+},{
+    id: 13,
+    img:'images-g\7.jpg',
+    name:'DP12',
+    price:65000,
+    description:'DP12 produced in the United States, how many shots it contains, firing system 12 shots, gun weight 4.2 kg',
+    type:'shotgun'
+},{
+    id: 14,
+    img:'images-g\8.jpg',
+    name:'Benelli M4',
+    price:81000,
+    description:'Enelli M4 made in Italy, loaded like that, automatic firing system, gun weight 3.82 kg, Benelli M4 gun can hold up to 8 rounds',
+    type:'shotgun'
+},{
+    id: 15,
+    img:'images-g\9.jpg',
+    name:'M24 – American',
+    price:45000,
+    description:'M24 sniper rifle of the United States Army, size 7.62 mm, length 1.092 mm, aiming weight 794 grams, sand leg weight 318 grams.',
+    type:'sniper'
+},{
+    id: 16,
+    img:'images-g\10.jpg',
+    name:'M110 SR25 – American',
+    price:35000,
+    description:'The SR 25 rifle (Stoner 25 model rifle) was developed by Reed Knight (owner of Knights Firearms Co) and Eugene Stoner (designed the M16 and Stoner 63 rifles among other things) during the 1990s',
+    type:'sniper'
+},{
+    id: 17,
+    img:'images-g\11.jpg',
+    name:'L42 EnField – British',
+    price:18000,
+    description:'Lee Enfield is a sniper that is measured in one shot only. which is only for experienced snipers.',
+    type:'sniper'
+},{
+    id: 18,
+    img:'images-g\12.jpg',
+    name:'M14 – American',
+    price:8390,
+    description:'This gun was used by the US Army in the early days of the Vietnam War, but due to its heavy weight and large ammunition, it was not very portable and had a very powerful version of the last ammunition that was changed to the M-16 and the US Army.',
+    type:'sniper'
+},{
+    id: 19,
+    img:'images-g\13.jpg',
+    name:'Armalite M16',
+    price:35000,
+    description:'To the last model that is enough for children to study red blood or conscripts get familiar with some familiar faces. The M16 has been decommissioned a long time ago.',
+    type:'rifle'
+},{
+    id: 20,
+    img:'images-g\14.jpg',
+    name:'Heckler & Koch G36',
+    price:25500,
+    description:'The G36 from Germany, a half-present, half-futuristic look that long ago made an impression on those who played some shooting games.',
+    type:'rifle'
+},{
+    id: 21,
+    img:'images-g\15.jpg',
+    name:'Accuracy International AS50',
+    price:80000,
+    description:'In critical operations such as stopping convoys or any vehicle, a simple rifle may not be enough. AS50 British sniper rifle with.',
+    type:'rifle'
+},{
+    id: 22,
+    img:'images-g\16.jpg',
+    name:'IWI Galil ACE',
+    price:52000,
+    description:'Probably have seen each other with the AK familys relaxed shape in the middle of the gun looks similar.',
+    type:'rifle'
+},{
+    id: 23,
+    img:'images-a\1.jpg',
+    name:'9 mm.',
+    price:2500,
+    description:'Sold in a box.',
+    type:'pistol ammunition'
+},{
+    id: 24,
+    img:'images-a\2.jpg',
+    name:'.38',
+    price:2800,
+    description:'Sold in a box.',
+    type:'pistol ammunition'
+},{
+    id: 25,
+    img:'images-a\3.jpg',
+    name:'Winchester super x',
+    price:3000,
+    description:'Sold in a box.',
+    type:'shotgun shells'
+},{
+    id: 26,
+    img:'images-a\4.jpg',
+    name:'Eley',
+    price:3500,
+    description:'Sold in a box.',
+    type:'shotgun shells'
+},{
+    id: 27,
+    img:'images-a\5.jpg',
+    name:'Ball m33',
+    price:4550,
+    description:'Sold in a box.',
+    type:'sniper bullet'
+},{
+    id: 28,
+    img:'images-a\6.jpg',
+    name:'M17 tracer',
+    price:5500,
+    description:'Sold in a box.',
+    type:'sniper bullet'
+},{
+    id: 29,
+    img:'images-a\7.jpg',
+    name:'m.16',
+    price:2800,
+    description:'Sold in a box.',
+    type:'rifle ammunition'
+},{
+    id: 30,
+    img:'images-a\8.jpg',
+    name:'50 Cal BMG',
+    price:4500,
+    description:'Sold in a box.',
+    type:'rifle ammunition'
+}];
+
+
+$(document).ready(() => {
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        html += `<div class="card-body">
+                <img src="${product[i].img}" alt="ปืนพก">
+                <h5 class="card-title">${product[i].name}</h5>
+                <p class="card-text">A popular model and a popular brand from Austria, GLOCK, a well-known gun manufacturer that constantly releases new models.</p>
+                <a> ${product[i].price}THB</a>
+                </div>`;
+    }
+    $("#productlist").html(html);
+
+})
+
+function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
+function searchsomething(elem) {
+    // console.log('#'+elem.id)
+    var value = $('#'+elem.id).val()
+    console.log(value)
+
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        if( product[i].name.includes(value) ) {
+            html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                    <img class="product-img" src="${product[i].img}" alt="">
+                    <p style="font-size: 1.2vw;">${product[i].name}</p>
+                    <p stlye="font-size: 1vw;">${ numberWithCommas(product[i].price) } THB</p>
+                </div>`;
+        }
+    }
+    if(html == '') {
+        $("#productlist").html(`<p>Not found product</p>`);
+    } else {
+        $("#productlist").html(html);
+    }
+
+}
+
+function searchproduct(param) {
+    console.log(param)
+    $(".product-items").css('display', 'none')
+    if(param == 'all') {
+        $(".product-items").css('display', 'block')
+    }
+    else {
+        $("."+param).css('display', 'block')
+    }
+}
+
+var productindex = 0;
+function openProductDetail(index) {
+    productindex = index;
+    console.log(productindex)
+    $("#modalDesc").css('display', 'flex')
+    $("#mdd-img").attr('src', product[index].img);
+    $("#mdd-name").text(product[index].name)
+    $("#mdd-price").text( numberWithCommas(product[index].price) + ' THB')
+    $("#mdd-desc").text(product[index].description)
+}
+
+function closeModal() {
+    $(".modal").css('display','none')
+}
+
+function BuyModal() {
+    $(".modal").css('display','none')
+}
+
+var cart = [];
+function addtocart() {
+    var pass = true;
+
+    for (let i = 0; i < cart.length; i++) {
+        if( productindex == cart[i].index ) {
+            console.log('found same product')
+            cart[i].count++;
+            pass = false;
+        }
+    }
+
+    if(pass) {
+        var obj = {
+            index: productindex,
+            id: product[productindex].id,
+            name: product[productindex].name,
+            price: product[productindex].price,
+            img: product[productindex].img,
+            count: 1
+        };
+        // console.log(obj)
+        cart.push(obj)
+    }
+    console.log(cart)
+
+    Swal.fire({
+        icon: 'success',
+        title: 'Add ' + product[productindex].name + ' to cart !'
+    })
+    $("#cartcount").css('display','flex').text(cart.length)
+}
+
+function openCart() {
+    $('#modalCart').css('display','flex')
+    rendercart();
+}
+
+function rendercart() {
+    if(cart.length > 0) {
+        var html = '';
+        for (let i = 0; i < cart.length; i++) {
+            html += `<div class="cartlist-items">
+                        <div class="cartlist-left">
+                            <img src="${cart[i].img}" alt="">
+                            <div class="cartlist-detail">
+                                <p style="font-size: 1.5vw;">${cart[i].name}</p>
+                                <p style="font-size: 1.2vw;">${ numberWithCommas(cart[i].price * cart[i].count) } THB</p>
+                            </div>
+                        </div>
+                        <div class="cartlist-right">
+                            <p onclick="deinitems('-', ${i})" class="btnc">-</p>
+                            <p id="countitems${i}" style="margin: 0 20px;">${cart[i].count}</p>
+                            <p onclick="deinitems('+', ${i})" class="btnc">+</p>
+                        </div>
+                    </div>`;
+        }
+        $("#mycart").html(html)
+    }
+    else {
+        $("#mycart").html(`<p>Not found product list</p>`)
+    }
+}
+
+function deinitems(action, index) {
+    if(action == '-') {
+        if(cart[index].count > 0) {
+            cart[index].count--;
+            $("#countitems"+index).text(cart[index].count)
+
+            if(cart[index].count <= 0) {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Are you sure to delete?',
+                    showConfirmButton: true,
+                    showCancelButton: true,
+                    confirmButtonText: 'Delete',
+                    cancelButtonText: 'Cancel'
+                }).then((res) => {
+                  if(res.isConfirmed) {
+                     cart.splice(index, 1) 
+                     console.log(cart)
+                     rendercart();
+                     $("#cartcount").css('display','flex').text(cart.length)
+                     
+                     if(cart.length <= 0) {
+                        $("#cartcount").css('display','none')
+                     }
+                  }  
+                  else {
+                    cart[index].count++;
+                    $("#countitems"+index).text(cart[index].count)
+                    rendercart();
+                  }
+                })
+            }
+            rendercart();
+        }
+        
+    }
+    else if(action == '+') {
+        cart[index].count++;
+        $("#countitems"+index).text(cart[index].count)
+        rendercart();
+    }
+}
